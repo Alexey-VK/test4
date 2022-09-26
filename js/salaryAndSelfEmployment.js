0d345e2bf2aa230da834cfcabb9cb6728e6735f9
akucher <aleksey.kucher.external@igraal.com>
2022-09-26 12:35:42 +0700

    fixed currency

--- js/salaryAndSelfEmployment.js
+++ js/salaryAndSelfEmployment.js
@@ -98,7 +98,7 @@
     if (payments) {
       for (let year in payments) {
         for (let month in payments[year]) {
-          const {  efforts, accrued } = payments[year][month];
+          const {  efforts, accrued, currency } = payments[year][month];
           const { total, salary_currency } = accrued;
           //const { cards_1, cards_2, cards_3, cards_4 } = paid_llc;
           //const { inc_1, inc_2, inc_overpaid, prepayment } = paid_inc;
